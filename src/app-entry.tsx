d6de115a2497a2637b7dd7d27a67ff29b0b325dd
anuruddha96 <anuruddha.dharmasena@gmail.com>
2026-09-01 14:33:13 +0200

    Load revenue grid performance paint policy

--- src/app-entry.tsx
+++ src/app-entry.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./styles/revenue-grid-performance.css";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
 import { installGlobalErrorReporting } from "@/lib/clientErrorReporter";
 
